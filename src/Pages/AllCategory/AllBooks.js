47f99c659085c473fc0130c67aa08d375900f1cc
Mollika-deb <108429309+Mollika-deb@users.noreply.github.com>
2022-11-26 21:17:03 +0600

    modal added

--- src/Pages/AllCategory/AllBooks.js
+++ src/Pages/AllCategory/AllBooks.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
 const AllBooks = ({ product }) => {
-    const { book_title } = product
+    const { book_title, image_url, location, resale_price, original_peice, use_year, post_time } = product
     return (
-        <div className="card  bg-slate-800 shadow-2xl">
+        <div className="card mt-10 bg-slate-800 shadow-2xl">
             <figure className="px-10 pt-10">
-                <img src='' alt="Shoes" className="rounded-xl h-64 w-64" />
+                <img src={image_url} alt="Shoes" className="rounded-xl h-64 w-64" />
             </figure>
             <div className="card-body items-center text-center">
-                <h2 className="card-title text-cyan-500">{book_title}</h2>
-
-                <button className="btn bg-pink-600 w-64">See All</button>
+                <h2 className="card-title font-bold text-green-600">{book_title}</h2>
+            
+            <div className="flex justify-between">
+                    <p className='font-semibold text-orange-500'>used: {use_year} years</p>
+                    <p className='ml-5 font-semibold text-orange-500'>post: {post_time} </p>
             </div>
+            <div className="flex  justify-between">
+                    <p className=' font-semibold text-orange-500'>Buying price: {original_peice}</p>
+                <p className='ml-5 font-semibold text-orange-500'>selling price:  {resale_price}</p>
+            </div>
+            
+                <p className='font-bold text-green-500'>location: {location}</p>
+                <button className="btn bg-pink-600 w-64">Book Now</button>
+              </div>
+          
 
         </div>
     );
--- src/Pages/AllCategory/AllCategory.js
+++ src/Pages/AllCategory/AllCategory.js
@@ -6,7 +6,7 @@ import AllBooks from './AllBooks';
 const AllCategory = () => {
     const products = useLoaderData()
     return (
-        <div>
+        <div className='grid md:grid-cols2 lg:grid-cols-3 gap-8 mb-10 mr-32 ml-32'>
            {
             products.map(product => <AllBooks id=
                 {product._id}
