80c699fee8043c23ada225d7485308fb5614bf09
Mollika-deb <108429309+Mollika-deb@users.noreply.github.com>
2022-11-27 20:48:13 +0600

    react-query added

--- src/Pages/NewDashboard/MyOrders/MyOrders.js
+++ src/Pages/NewDashboard/MyOrders/MyOrders.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useQuery } from 'react-query';
+import { AuthContext } from '../../../Context/AuthProvider/AuthProvider';
+import OrderCard from './OrderCard';
 
 const MyOrders = () => {
+    const {user} = useContext(AuthContext);
+
+     const url = `http://localhost:5000/order?email=${user?.email}`;
+    const {data : orders = []} = useQuery({
+        queryKey:['order', user?.email],
+        queryFn: async () =>{
+            const res = await fetch(url);
+            const data = await res.json();
+            return data;
+        }
+
+    })
+
     return (
         <div>
-            
+            {
+                orders.map(order =><OrderCard
+                    kye={order._id}
+                    order = {order}
+                    ></OrderCard>)
+            }
         </div>
     );
 };
---
+++ src/Pages/NewDashboard/MyOrders/OrderCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const OrderCard = ({order}) => {
+    const { image_url, book_title, resale_price, location  } = order
+    return (
+        <div className="card mt-10 bg-slate-800 shadow-2xl">
+            <figure className="px-10 pt-10">
+                <img src={image_url} alt="Shoes" className="rounded-xl h-64 w-64" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title font-bold text-green-600">{book_title}</h2>
+
+                
+                
+                    <p className='ml-5 font-semibold text-orange-500'>selling price:  {resale_price}</p>
+                </div>
+
+                <p className='font-bold text-green-500'>location: {location}</p>
+
+                <label  htmlFor="booking-modal" className="btn bg-pink-600 w-64">Book Now</label>
+            </div>
+
+
+        
+    );
+};
+
+export default OrderCard;
